19385e5d8b5f45228c5b11759bf37863cdd8cbba
szymon.szedel <szymon.szedel@almservices.pl>
2023-08-26 14:58:46 +0200

    divide one function into more smaller ones

--- src/main.ts
+++ src/main.ts
@@ -13,32 +13,35 @@ const path: string = args[2]
 const newPath: string = pth.join(path, args[3])
 console.log(newPath)
 
-fs.readdir(path, (err: NodeJS.ErrnoException | null, files: string[]): void => {
+fs.readdir(path, readDirectoryCallback)
+
+function readDirectoryCallback (err: NodeJS.ErrnoException | null, files: string[]): void {
     if(err) {
         console.log(err)
         return
     }
     for(let i: number = 0; i < files.length; i++) {
-        fs.readFile(pth.join(path, files[i]), (err: NodeJS.ErrnoException | null, data: Buffer) => {
-            if(err) {
-                console.log(err)
-                return
-            }
-            let newFilePath: pth.ParsedPath = pth.parse(pth.join(newPath, files[i]))
-            newFilePath.base = newFilePath.name + '.txt'
-            fs.mkdir(newFilePath.dir, (err: NodeJS.ErrnoException | null): void => {
-                if (err) {
-                    console.log(err)
-                    return
-                }
-            })
-            let a = pth.format(newFilePath)
-            fs.writeFile(a , data, (err: NodeJS.ErrnoException | null): void => {
-                if (err) {
-                    console.log(err)
-                    return
-                }
-            })
-        })
+        fs.readFile(pth.join(path, files[i]), readFileCallback(files[i]))
     }
-})
+}
+
+function readFileCallback (file: string): (err: NodeJS.ErrnoException | null, data: Buffer) => void {
+    return (err, data) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+        let newFilePath: pth.ParsedPath = pth.parse(pth.join(newPath, file))
+        newFilePath.base = newFilePath.name + '.txt'
+        fs.mkdir(newFilePath.dir, errorHandleCallback)
+        let a = pth.format(newFilePath)
+        fs.writeFile(a , data, errorHandleCallback)
+    }
+}
+
+function errorHandleCallback (err: NodeJS.ErrnoException | null): void {
+    if (err) {
+        console.log(err)
+        return
+    }
+}
