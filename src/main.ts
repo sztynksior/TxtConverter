c57c3830da9d8b3c02dd5c122f5060953b4f7357
szymon.szedel <szymon.szedel@almservices.pl>
2023-08-26 16:13:34 +0200

    refactore code

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,43 @@
 import * as fs from 'fs'
 import * as pth from 'path'
+import * as pc from './utils/path-convert'
 
 const args: string[] = process.argv
 const numArgs: number = args.length
 
-if (numArgs != 4) {
-    console.log("arguments: [directory name] [directory name for converted data]")
+if (numArgs != 3) {
+    console.log("arguments: [directory name]")
     process.exit()
 }
 
 const path: string = args[2]
-const newPath: string = pth.join(path, args[3])
-console.log(newPath)
 
-fs.readdir(path, readDirectoryCallback)
+fs.readdir(path, readAllFilesInDirecotry(path))
 
-function readDirectoryCallback (err: NodeJS.ErrnoException | null, files: string[]): void {
-    if(err) {
-        console.log(err)
-        return
-    }
-    for(let i: number = 0; i < files.length; i++) {
-        fs.readFile(pth.join(path, files[i]), readFileCallback(files[i]))
+function readAllFilesInDirecotry (path: string): (err: NodeJS.ErrnoException | null, files: string[]) => void {
+    return (err, files) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+        for(let i: number = 0; i < files.length; i++) {
+            const filePath: string = pth.join(path, files[i])
+            fs.readFile(filePath, saveFileData(pc.changeFilePathExtension(filePath, ".txt")))
+        }
     }
 }
 
-function readFileCallback (file: string): (err: NodeJS.ErrnoException | null, data: Buffer) => void {
+function saveFileData (filePath: string): (err: NodeJS.ErrnoException | null, data: Buffer) => void {
     return (err, data) => {
         if(err) {
             console.log(err)
             return
         }
-        let newFilePath: pth.ParsedPath = pth.parse(pth.join(newPath, file))
-        newFilePath.base = newFilePath.name + '.txt'
-        fs.mkdir(newFilePath.dir, errorHandleCallback)
-        let a = pth.format(newFilePath)
-        fs.writeFile(a , data, errorHandleCallback)
+        fs.writeFile(filePath, data, handleError)
     }
 }
 
-function errorHandleCallback (err: NodeJS.ErrnoException | null): void {
+function handleError (err: NodeJS.ErrnoException | null): void {
     if (err) {
         console.log(err)
         return
